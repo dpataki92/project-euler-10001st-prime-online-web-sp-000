977d3953bad9367fbbcdb5d32a9b43283cb3cc0e
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-12-11 19:01:20 +0100

    Done.

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,19 @@
 # Implement your procedural solution here!
+def prime_number_for(num)
+    primes = []
+    counter = 2
+
+    while primes.size < num
+        if counter === 2 || counter === 3
+            primes << counter
+            counter += 1
+        elsif !(2..Math.sqrt(counter).floor).any? {|n| counter % n === 0}
+            primes << counter
+            counter += 1
+        else
+            counter += 1
+        end
+    end
+
+    primes[primes.size-1]
+end
--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,25 @@
 # Implement your object-oriented solution here!
+class Prime
+    def initialize(num)
+        @num = num
+    end
+
+    def number
+        primes = []
+        counter = 2
+    
+        while primes.size < @num
+            if counter === 2 || counter === 3
+                primes << counter
+                counter += 1
+            elsif !(2..Math.sqrt(counter).floor).any? {|n| counter % n === 0}
+                primes << counter
+                counter += 1
+            else
+                counter += 1
+            end
+        end
+    
+        primes[primes.size-1]
+    end
+end
